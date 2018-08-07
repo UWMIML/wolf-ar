84bdbd20f097c3c8c75f9457578c708dcc5b76b6
Joseph Rex <joerexko@gmail.com>
2018-08-07 10:56:18 -0500

    Add setup

---
+++ README.md
@@ -0,0 +1,18 @@
+# WEBVR Planets
+A showcase of earth, moon, and sun in webVR with THREE.js. This file also serves as a template for webVR projects.
+
+## Installing and Running
+Download and [install nodejs] if you don't already have it. Then run:
+```
+npm i
+```
+to install dependencies. To start the local server, run:
+```
+npm start
+```
+and the webpage should be visible from http://localhost:1234/
+
+## Demo
+Demo of this can be seen at https://uwmiml.github.io/webvr-planets
+
+[1]: https://nodejs.org/en/download
---
+++ src/script.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three';
+import GLTFLoader from 'three-gltf-loader';
+import WEBVR from './WebVR';
+import riggedWolfGLTF from './rigged-wolf.gltf';
+
+const ready = cb => {
+  /in/.test(document.readyState) // in = loadINg
+    ? setTimeout(ready.bind(null, cb), 9)
+    : cb();
+}
+
+// Reset camera and renderer on resize
+const windowResize = (renderer, camera) => {
+  const [ windowWidth, windowHeight ] = [ window.innerWidth, window.innerHeight ];
+  camera.aspect = windowWidth / windowHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(windowWidth, windowHeight);
+};
+
+ready(function() {
+  const [ windowWidth, windowHeight ] = [ window.innerWidth, window.innerHeight ];
+  // Set up renderer
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(windowWidth, windowHeight);
+  renderer.vr.enabled = true;
+  renderer.vr.userHeight = 0;
+  document.body.appendChild(renderer.domElement);
+  document.body.appendChild( WEBVR.createButton( renderer, { frameOfReferenceType: 'eye-level' } ) );
+
+  // Set up scene
+  const scene = new THREE.Scene();
+
+  // Add light to scene
+  const light = new THREE.HemisphereLight(0xfff0f0, 0x606066)
+  light.position.set(1, 1, 1);
+  scene.add(light);
+
+  // Add camera
+  const camera = new THREE.PerspectiveCamera(45, windowWidth / windowHeight, 1, 1000);
+  camera.position.set(0, 0, 60);
+
+  // Update dimensions on resize
+  windowResize(renderer, camera)
+
+  // Prepare geometries and meshes
+  const loader = new GLTFLoader();
+  loader.load(riggedWolfGLTF, gltf => {
+    console.log("Test whole gltf", gltf);
+    const children = [];
+    gltf.scene.traverse(child => {
+      console.log(child);
+
+      if(child.isMesh){
+        console.log("Found child mesh");
+        children.push(child);
+      }
+    })
+    children.forEach(child => scene.add(child));
+    // scene.add(gltf.scene);
+  });
+  // render scene
+  const render = () => {
+    renderer.render(scene, camera);
+  };
+  renderer.setAnimationLoop(render);
+});
